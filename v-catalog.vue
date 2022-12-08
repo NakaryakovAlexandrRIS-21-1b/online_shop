9660be8966a14fa08e5cf355d82a62e9bc7f59a8
DmitryErmakoff <117264406+DmitryErmakoff@users.noreply.github.com>
2022-12-08 07:46:52 +0500

    Add files via upload

---
+++ v-catalog.vue
@@ -0,0 +1,50 @@
+<template >
+    <div class = "v-catalog">
+        <h1>Catalog</h1>
+        <div class="selector">
+            <select 
+            class = "select" >
+            </select>
+        </div>
+        <div class = "catalog-list">
+            <v-catalog-item 
+                
+            />
+
+        </div>
+    </div>
+    
+</template>
+
+<script>
+    import vCatalogItem from "./v-catalog-item"
+
+    export default {
+        name: "v-catalog",
+        components: {
+            vCatalogItem
+        },
+        computed: {
+
+        },
+        props: 
+            ['prod'],
+        methods: {
+        
+        },
+        
+        data() {
+            return {
+                sortedCatalog: [],
+                selected:"All",
+                options:[
+                    "All",
+                    "Male",
+                    "Female"
+                ]
+            }
+        }
+    }
+</script>
+
+    
