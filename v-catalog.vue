c883b3800e29ee37b961617936cc3a3c59d1a322
Накаряков Александр РИС-21-1б <92480115+NakaryakovAlexandrRIS-21-1b@users.noreply.github.com>
2022-12-08 07:55:25 +0500

    Add files via upload

--- v-catalog.vue
+++ v-catalog.vue
@@ -3,12 +3,23 @@
         <h1>Catalog</h1>
         <div class="selector">
             <select 
-            class = "select" >
+            class = "select"
+            v-model = "selected"
+            @change="SortByFilter">
+                <option 
+                v-for = "option in options"
+                :key = "option"
+                :value = "option"
+                >{{option}}
+                </option>
             </select>
         </div>
         <div class = "catalog-list">
             <v-catalog-item 
-                
+                v-for = "product in sortedCatalog" 
+                :key="product.article"
+                :product_data = "product"
+                @GetCart="GetCart"
             />
 
         </div>
@@ -30,9 +41,25 @@
         props: 
             ['prod'],
         methods: {
-        
+            SortByFilter(){
+                let vm = this
+                this.sortedCatalog = []
+                this.prod.map(function(item){
+                    if(vm.selected != "All"){
+                        if(item.category == vm.selected){
+                            vm.sortedCatalog.push(item)
+                        }
+                    }
+                    else{
+                        vm.sortedCatalog.push(item)
+                    }
+                })
+            },
+            
         },
-        
+        mounted(){
+            this.sortedCatalog = this.prod
+        },
         data() {
             return {
                 sortedCatalog: [],
