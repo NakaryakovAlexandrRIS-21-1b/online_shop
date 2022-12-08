679722cfdd060b002a6dc76072b1d2d19aa7fec8
DmitryErmakoff <117264406+DmitryErmakoff@users.noreply.github.com>
2022-12-08 07:45:37 +0500

    Add files via upload

---
+++ v-main-wrapper.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="v-main-wrapper">
+        <div class = "cart_catalog">
+                Cart
+        </div>
+        <div 
+            class = "cart_catalog">
+                Back to catalog
+        </div>
+        <div >Loading...</div>
+        <v-catalog 
+            
+        />
+        <v-cart 
+            
+         />
+    </div>
+</template>
+
+<script>
+import vCatalog from "./v-catalog"
+import vCart from "./v-cart"
+
+export default{
+    name: 'v-main-wrapper',
+    components: {
+        vCatalog,
+       vCart
+    },
+    
+    data() {
+        return {
+            IsCart: false,
+            products: [
+            ],
+            cart_items: [
+            ]
+        }
+    },
+}
+</script>
+
+<style>
+    
+</style>
