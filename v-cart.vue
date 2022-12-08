adaf8713be36ee8d73775fd97c6d56c62cfddbce
DmitryErmakoff <117264406+DmitryErmakoff@users.noreply.github.com>
2022-12-08 07:47:59 +0500

    Add files via upload

---
+++ v-cart.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class = "v-cart" >
+        <h1>Cart</h1>
+        <div 
+            class = "NoCart">
+                There are no items in cart...
+        </div>
+        <div class = "v-item">
+            <v-cart-item 
+            
+            />
+        </div>
+        <div class = "Total">
+            <h1>
+                Total: 0 ₽
+            </h1>
+        </div>
+    </div>
+</template>
+<script>
+import vCartItem from './v-cart-item'
+export default {
+    name: "v-cart",
+    components: {
+        vCartItem
+    },
+    computed: {
+      
+    },
+    props: {
+       
+    },
+    methods:{
+       
+    },
+
+    data() {
+        return {
+        }
+    }
+}
+</script>
+
+
