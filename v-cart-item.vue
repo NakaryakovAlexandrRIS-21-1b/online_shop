0924995172d614232f20efff479519e28c7597c4
Накаряков Александр РИС-21-1б <92480115+NakaryakovAlexandrRIS-21-1b@users.noreply.github.com>
2022-12-08 07:55:02 +0500

    Add files via upload

--- v-cart-item.vue
+++ v-cart-item.vue
@@ -2,33 +2,52 @@
     <div class = "v-cart-item">
         <div class = "v-cart-item img">
             <img 
-                :src="../assets/images/" 
+                :src="require('../assets/images/' + product_data.image)" 
                 alt="img" 
                 class = "v-catalog-item-img"
             >
         </div>
         <div class = "v-item-info" >
-            <p class = "product-name"></p>
-            <p class = "product-price">Price:  ₽</p>
+            <p class = "product-name">{{product_data.name}}</p>
+            <p class = "product-price">Price: {{product_data.price}} ₽</p>
         </div>
         <div class = "v-item-quantity">
             <p>quantity</p>
-            <button >+</button>
+            <button @click = "PlusItem(product_data.idx)">+</button>
             <p>{{product_data.quantity}}</p>
-            <button >-</button>
+            <button @click = "MinusItem(product_data.idx)">-</button>
         </div>
         <div>
-            <button >Delete</button>
+            <button @click = "DeleteFromCart(product_data.idx)">Delete</button>
         </div>
     </div>
 </template>
 <script>
 export default {
     props: {
-        
+        product_data: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
     },
     methods:{
-       
+        
+        async DeleteFromCart(idx){
+            await fetch(`http://localhost/queries/deleteFromCart.php?idx=${idx}`)
+            await this.$emit("deleted")
+        },
+        async PlusItem(idx){
+            await fetch(`http://localhost/queries/addQuantity.php?idx=${idx}`)
+            await this.$emit("deleted")
+            
+        },
+        async MinusItem(idx){
+            await fetch(`http://localhost/queries/minusQuantity.php?idx=${idx}`)
+            await this.$emit("deleted")
+            
+        }
     }
 
 }
