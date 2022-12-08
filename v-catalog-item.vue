b04eea600d30041e908b139bee6f93c23c69beee
Накаряков Александр РИС-21-1б <92480115+NakaryakovAlexandrRIS-21-1b@users.noreply.github.com>
2022-12-08 07:55:56 +0500

    Add files via upload

--- v-catalog-item.vue
+++ v-catalog-item.vue
@@ -2,35 +2,40 @@
     <div class = "v-catalog-item">
         <v-popup 
         class = "popup"
-       
+        v-if = "PopupVisible"
+        :added = "Added"
+        @Close = "ClosePopup"
+        @AddToCart = "productClick(product_data.idx)"
         >
          <div class="popup-background">
             <div class = "popup-wrapper">
                 <img 
-                    :src="../assets/images" 
+                    :src="require('../assets/images/' + product_data.image)" 
                     alt="img" 
                     class = "v-catalog-item-img">
                 <div class = "popup-context">
-                    <h3 class = "product-name-pop"></h3>
-                    <p class = "product-context"></p>
+                    <h3 class = "product-name-pop">{{product_data.name}}</h3>
+                    <p class = "product-context">{{product_data.context}} </p>
                 </div>
             </div>
         </div>
         </v-popup>
         <img 
-            :src="../assets/images/" 
+            :src="require('../assets/images/' + product_data.image)" 
             alt="img" 
             class = "v-catalog-item-img">
-        <p class = "product-name"></p>
-        <p class = "product-price">Price: ₽</p>
+        <p class = "product-name">{{product_data.name}}</p>
+        <p class = "product-price">Price: {{product_data.price}} ₽</p>
         <div class = "buttons">
             <button
                 class = "show-info btn"
-                
+                @click="ShowPopup"
                 >Show info
             </button>
         <button 
-            class = "add-to-cart-btn btn">
+            class = "add-to-cart-btn btn" 
+            @click ="productClick(product_data.idx)"
+                >{{Added}}
         </button>
         </div>
         
@@ -47,7 +52,27 @@ export default {
 
     },
     methods: {
-    
+    productClick(idx) {
+        fetch(`http://localhost/queries/addToCart.php?idx=${idx}`)
+        this.Added = "Also in cart"
+
+      },
+    ShowPopup(){
+        this.PopupVisible = true
+    },
+    ClosePopup(){
+        this.PopupVisible = false    
+    },
+    },
+    props: {
+        product_data: {
+            type: Object,
+            default() {
+                return {}
+            }
+        }
+    },
+
     data() {
         return {
             PopupVisible: false,
