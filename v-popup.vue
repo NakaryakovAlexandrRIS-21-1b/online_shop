46178e7bfdb90b97ecb6f888db382ce9b2240860
DmitryErmakoff <117264406+DmitryErmakoff@users.noreply.github.com>
2022-12-08 07:52:46 +0500

    Add files via upload

---
+++ v-popup.vue
@@ -0,0 +1,26 @@
+<template >
+    <div class = "popup">
+        <div class="popup_content">
+            <slot></slot>
+        </div>
+        <div class="popup_footer">
+            <button class="ClosePopup"
+            >Close</button>
+            <button class = "AddToCart"
+            ></button>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+
+    data(){
+        return {
+            
+        }
+    }
+}
+</script>
+
