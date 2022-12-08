94afef02141f6c6330a7a1c832e8f808a1da737f
Накаряков Александр РИС-21-1б <92480115+NakaryakovAlexandrRIS-21-1b@users.noreply.github.com>
2022-12-08 07:53:38 +0500

    Add files via upload

--- v-cart.vue
+++ v-cart.vue
@@ -2,17 +2,23 @@
     <div class = "v-cart" >
         <h1>Cart</h1>
         <div 
+            v-if="!prod.length" 
             class = "NoCart">
                 There are no items in cart...
         </div>
         <div class = "v-item">
             <v-cart-item 
-            
+                v-for = "(product, index) in prod"
+                :key = "product.article"
+                :product_data = "product"
+                @deleted = "Delet"
+                @PlusItem = "PlusItem(index)"
+                @MinusItem = "MinusItem(index)"
             />
         </div>
         <div class = "Total">
             <h1>
-                Total: 0 ₽
+                Total: {{CartCost}} ₽
             </h1>
         </div>
     </div>
@@ -25,13 +31,28 @@ export default {
         vCartItem
     },
     computed: {
-      
+        CartCost(){
+            let Cost = 0
+            for(let product of this.prod){
+                Cost += product.price*product.quantity
+            }
+            return Cost
+        },
+        
     },
     props: {
-       
+        prod: {
+            type: Object,
+                default(){
+                    return {}
+                }
+        },
+        
     },
     methods:{
-       
+        Delet(){
+            this.$emit('getCart')
+        },
     },
 
     data() {
